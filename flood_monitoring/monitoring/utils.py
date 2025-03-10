3031e9bcb7ab47369d7f150f46de40fac61956f6
softplanner <78175346+softplanner@users.noreply.github.com>
2025-03-10 06:29:03 +0100

    Limited Readings Over Last 24 Hours

--- flood_monitoring/monitoring/utils.py
+++ flood_monitoring/monitoring/utils.py
@@ -3,4 +3,4 @@ from monitoring.config import STATIONS_ENDPOINT
 class Utils:    
     @staticmethod
     def get_readings_endpoint(station_id):
-        return f"{STATIONS_ENDPOINT}/{station_id}/readings?_sorted"
+        return f"{STATIONS_ENDPOINT}/{station_id}/readings?_sorted&_limit=96"
